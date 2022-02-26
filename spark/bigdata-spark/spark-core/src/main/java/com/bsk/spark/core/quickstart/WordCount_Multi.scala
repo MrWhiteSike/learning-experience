89b5f21d2928b19bf6cd040eb78c12607a0c75b4
MrWhiteSike <mrwhitesike@gmail.com>
2022-02-26 22:30:04 +0800

    Operator Action and Serializable

--- spark/bigdata-spark/spark-core/src/main/java/com/bsk/spark/core/quickstart/WordCount_Multi.scala
+++ spark/bigdata-spark/spark-core/src/main/java/com/bsk/spark/core/quickstart/WordCount_Multi.scala
@@ -2,18 +2,23 @@ package com.bsk.spark.core.quickstart
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.collection.mutable
+
 object WordCount_Multi {
 
   def main(args: Array[String]): Unit = {
 
     val sc = new SparkContext(new SparkConf().setMaster("local").setAppName("WordCount"))
-    wordcount(sc)
-
-
-
+    wordcount11(sc)
     sc.stop()
 
   }
+
+  /*
+    求 wordcount 的 11 种方法：
+    groupBy、groupByKey、reduceByKey、aggregateByKey、foldByKey、combineByKey、
+    countByKey、countByValue、reduce、aggregate、fold
+   */
 
   // groupBy
   def wordcount(sc: SparkContext): Unit ={
@@ -86,6 +91,100 @@ object WordCount_Multi {
     val rdd = sc.makeRDD(List("Hello Scala", "Hello Spark"))
     val mapRDD = rdd.flatMap(_.split(" "))
     val wordcount = mapRDD.countByValue()
+  }
+
+  // reduce
+  def wordcount9(sc: SparkContext): Unit ={
+    val rdd = sc.makeRDD(List("Hello Scala", "Hello Spark"))
+    val words = rdd.flatMap(_.split(" "))
+
+    // word => Map[(word,1)]
+    val mapRDD = words.map(
+      word => {
+        // mutable 操作起来比较方便
+        mutable.Map[String, Long]((word, 1))
+      }
+    )
+
+    // Map 和 Map 聚合
+    val wordcount = mapRDD.reduce(
+      (map1, map2) => {
+        map2.foreach {
+          case (word, count) => {
+            val newCount = map1.getOrElse(word, 0L) + count
+            map1.update(word, newCount)
+          }
+        }
+        map1
+      }
+    )
+    println(wordcount)
+  }
+
+  // aggregate
+  def wordcount10(sc: SparkContext): Unit ={
+    val rdd = sc.makeRDD(List("Hello Scala", "Hello Spark"))
+    val words = rdd.flatMap(_.split(" "))
+
+    // word => Map[(word,1)]
+    val mapRDD = words.map(
+      word => {
+        // mutable 操作起来比较方便
+        mutable.Map[String, Long]((word, 1))
+      }
+    )
+
+    // Map 和 Map 聚合
+    val wordcount = mapRDD.aggregate(mutable.Map[String, Long]((mapRDD.first().keySet.head,0)))(
+      (map1, map2) => {
+      map2.foreach {
+        case (word, count) => {
+          val newCount = map1.getOrElse(word, 0L) + count
+          map1.update(word, newCount)
+        }
+      }
+      map1
+    }, (map1, map2) => {
+        map2.foreach {
+          case (word, count) => {
+            val newCount = map1.getOrElse(word, 0L) + count
+            map1.update(word, newCount)
+          }
+        }
+        map1
+      }
+    )
+
+    println(wordcount)
+  }
+
+  // fold
+  def wordcount11(sc: SparkContext): Unit ={
+    val rdd = sc.makeRDD(List("Hello Scala", "Hello Spark"))
+    val words = rdd.flatMap(_.split(" "))
+
+    // word => Map[(word,1)]
+    val mapRDD = words.map(
+      word => {
+        // mutable 操作起来比较方便
+        mutable.Map[String, Long]((word, 1))
+      }
+    )
+
+    // Map 和 Map 聚合
+    val wordcount = mapRDD.fold(mutable.Map[String, Long]((mapRDD.first().keySet.head,0)))(
+      (map1, map2) => {
+        map2.foreach {
+          case (word, count) => {
+            val newCount = map1.getOrElse(word, 0L) + count
+            map1.update(word, newCount)
+          }
+        }
+        map1
+      }
+    )
+
+    println(wordcount)
   }
 
 
