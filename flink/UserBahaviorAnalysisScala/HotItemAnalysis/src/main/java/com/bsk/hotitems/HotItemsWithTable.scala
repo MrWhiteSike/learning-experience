2888f4d53955cfda2f6ac94ff0431d3f29669cb7
MrWhiteSike <mrwhitesike@gmail.com>
2023-06-19 13:57:05 +0800

    使用Table API实现热门商品TOP N

--- flink/UserBahaviorAnalysisScala/HotItemAnalysis/src/main/java/com/bsk/hotitems/HotItemsWithTable.scala
+++ flink/UserBahaviorAnalysisScala/HotItemAnalysis/src/main/java/com/bsk/hotitems/HotItemsWithTable.scala
@@ -41,30 +41,72 @@ object HotItemsWithTable {
 
     val table1 = table
       .filter($"behavior" === "pv")
-      .window(Slide over 1.hour every 5.minutes on $"ts" as $"w")
-      .groupBy($"w", $"itemId")
+      .window(Slide over 1.hours every 5.minutes on $"ts" as $"w")
+      .groupBy($"itemId", $"w")
       .select(
         $"itemId",
-        $"w".end as "endWindow",
+        $"w".end() as "rt",
         $"itemId".count as "cnt"
       )
 
+    table1.printSchema()
+
+    //TODO Table 是否可以直接进行表结构的变更？ 肯定是可以的
+    // 方式1；
+    val table2 = table1.addColumns($"rt".cast(DataTypes.TIMESTAMP_LTZ(3)).as("windowEnd")).dropColumns($"rt")
+
+    // 方式2：
+//    val table1Stream = tEnv.toDataStream(table1)
+//    val table2 = tEnv.fromDataStream(table1Stream,
+//      Schema
+//        .newBuilder()
+//        .columnByExpression("windowEnd", "cast(rt as TIMESTAMP_LTZ(3))")
+//        .build()
+//    )
+    table2.printSchema()
 
 
-    tEnv.createTemporaryView("table1", table1)
+    /**
+     * 问题：直接使用rt的时候出现如下异常
+     * Exception in thread "main" java.lang.AssertionError: Conversion to relational algebra failed to preserve datatypes:
+     * validated type:
+     * RecordType(BIGINT NOT NULL itemId, TIMESTAMP(3) rt, BIGINT NOT NULL cnt) NOT NULL
+     * converted type:
+     * RecordType(BIGINT NOT NULL itemId, TIMESTAMP_LTZ(3) rt, BIGINT NOT NULL cnt) NOT NULL
+     *
+     * 解决：修改table1的表结构，增加数据类型为TIMESTAMP_LTZ(3)的列作为分区列
+     *
+     * table1.addColumns($"rt".cast(DataTypes.TIMESTAMP_LTZ(3)).as("windowEnd"))
+     *
+     *
+     *
+     */
 
+
+    // 将聚合结果表注册到环境，写SQL实现top N
+    tEnv.createTemporaryView("table1", table2)
     val result = tEnv.sqlQuery(
       """
-        |select *
+        |select itemId,windowEnd,cnt,rn
         |from (
-        |   select *,row_number() over(partition by endWindow order by cnt desc) as rn
+        |   select itemId,windowEnd,cnt,row_number() over(partition by windowEnd order by cnt desc) as rn
         |   from table1
         |)
         |where rn <= 5
         |""".stripMargin)
 
-    tEnv.toDataStream(table1, classOf[Row]).print("table1")
-    tEnv.toDataStream(result, classOf[Row]).print("result")
+//    tEnv.toDataStream(table1, classOf[Row]).print("table1")
+
+
+    /**
+     * ROW_NUMBER 排序 是更新流，不能使用toDataStream 进行流表转换，需要使用toChangelogStream
+     * Exception in thread "main" org.apache.flink.table.api.TableException:
+     * Table sink '*anonymous_datastream_sink$4*' doesn't support consuming update and delete changes
+     * which is produced by node Rank(strategy=[UndefinedStrategy], rankType=[ROW_NUMBER],
+     * rankRange=[rankStart=1, rankEnd=5], partitionBy=[$2], orderBy=[cnt DESC], select=[itemId, cnt, $2, rn])
+     */
+//    tEnv.toDataStream(result, classOf[Row]).print("result")
+    tEnv.toChangelogStream(result).print("result")
 
 
     env.execute()
